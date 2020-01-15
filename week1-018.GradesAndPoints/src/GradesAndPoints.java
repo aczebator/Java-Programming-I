ff6b2a4b31aa211c9e7d0b9d232230614a54fd51
aczebator <48531433+aczebator@users.noreply.github.com>
2020-01-14 16:27:54 -0800

    Add files via upload

--- week1-018.GradesAndPoints/src/GradesAndPoints.java
+++ week1-018.GradesAndPoints/src/GradesAndPoints.java
@@ -5,26 +5,6 @@ public class GradesAndPoints {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-System.out.print("Type the points [0-60]:");
-        int number = Integer.parseInt(reader.nextLine()); 
-        if (number > 49) {
-           System.out.print("Grade: 5"); 
-        }
-        else if (number>44){
-            System.out.print("Grade: 4"); 
-        }
-        else if (number>39){
-            System.out.print("Grade: 3"); 
-        }
-        else if (number>34){
-            System.out.print("Grade: 2"); 
-        }
-        else if (number>29){
-            System.out.print("Grade: 1"); 
-        }
-        else {
-        System.out.print("Grade: failed"); 
-        }
-        
+
     }
 }
--- week1-019.AgeCheck/src/AgeCheck.java
+++ week1-019.AgeCheck/src/AgeCheck.java
@@ -5,13 +5,6 @@ public class AgeCheck {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-System.out.print("How old are you?");
-        int number = Integer.parseInt(reader.nextLine()); 
-        if (number < 121 && number > -1) {
-           System.out.print("Ok"); 
-        }
-        else {
-        System.out.print("Impossible!"); 
-        }
+
     }
 }
